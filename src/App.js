bb2732a234a247e538c98f0bbaedd74ba4d79821
Bogdan <bogdan7morozyuk@gmail.com>
2019-09-28 19:03:26 +0300

    Apply default layout for application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import DefaultLayout from 'components/layout/DefaultLayout';
 
 import 'App.css';
 
 const App = () => (
-  <div className="App"></div>
+  <div className="App">
+    <DefaultLayout />
+  </div>
 );
 
 export default App;
